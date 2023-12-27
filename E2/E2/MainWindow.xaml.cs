206fe5502b968a24dab5886a550eafe1824f0df4
unknown <zee.mj26@hotmail.com>
2023-12-27 20:34:13 +0500

    213001

---
+++ E2/E2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.Name == "checkBox1") 
+                textBox1.Text = "2 state CheckBox is checked.";
+            else 
+                textBox2.Text = "3 state CheckBox is checked.";
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.Name == "checkBox1") 
+                textBox1.Text = "2 state CheckBox is unchecked.";
+            else 
+                textBox2.Text = "3 state CheckBox is unchecked.";
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            textBox2.Text = "3 state CheckBox is in indeterminate state.";
+        }
+    }
+}
